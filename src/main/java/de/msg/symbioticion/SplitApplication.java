1f5f34411aa23858c30e7f7db56e0819e4c48e97
xgxtwow <wolfgang.werner@msg-gillardon.de>
2016-11-15 09:53:05 +0100

    change packaging to war, but it is still runnable standalone

--- src/main/java/de/msg/symbioticion/SplitApplication.java
+++ src/main/java/de/msg/symbioticion/SplitApplication.java
@@ -2,9 +2,16 @@ package de.msg.symbioticion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SplitApplication {
+public class SplitApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SplitApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SplitApplication.class, args);
