de7a05afd64332954056c269bdc1d571c7f34985
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-11-15 11:32:31 +0100

    Added log to DummyController.

--- src/main/java/de/msg/symbioticion/api/DummyController.java
+++ src/main/java/de/msg/symbioticion/api/DummyController.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import de.msg.symbioticion.domain.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,14 +15,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Rafael Kansy
  */
 @Controller
 @RequestMapping(value = "/dummy/")
+@Slf4j
 public class DummyController {
     private static final String DATABASE_URL = "https://symbioticon-fbf9e.firebaseio.com/";
     private static DatabaseReference database;
@@ -29,25 +29,19 @@ public class DummyController {
 
     public DummyController() {
         try (
-                //InputStream stream = new FileInputStream("C:\\Users\\kansyr\\IdeaProjects\\Pirat83\\firebase-example\\src\\main\\resources\\service-account.json");
                 InputStream stream = new FileInputStream("service-account.json");
         ) {
-            // [START initialize]
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setServiceAccount(stream)
                     .setDatabaseUrl(DATABASE_URL)
                     .build();
             FirebaseApp.initializeApp(options);
             stream.close();
-            // [END initialize]
 
         } catch (IOException e) {
-            System.out.println("ERROR: invalid service account credentials. See README.");
-            System.out.println(e.getMessage());
-            System.exit(1);
+            log.warn(e.getLocalizedMessage(), e);
         }
 
-        // Shared Database reference
         database = FirebaseDatabase.getInstance().getReference();
     }
 
@@ -55,20 +49,14 @@ public class DummyController {
     @RequestMapping(value = "users", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> createUsers() {
         database.getRoot().removeValue();
-        Map<String, String> userMap = new HashMap<>();
-
 
         for (int i = 0; i < 2; i++) {
             String firstName = i % 2 == 0 ? "rafael" : "wolfgang";
             String lastName = i % 2 == 0 ? "rafael" : "wolfgang";
-            String email = i % 2 == 0 ? "rafael.kansy@msg-systems.com" : "wolfgang.werner@msg-systems.com";
-            User user = new User("uid",firstName, lastName, "nickName", "DE527003250909", "BYLADEN1001");
+            User user = new User("uid", firstName, lastName, "nickName", "DE527003250909", "BYLADEN1001");
 
-            //database.getRoot().child("/users/").child(String.valueOf(i)).setValue(user);
             DatabaseReference key = database.getRoot().child("/users/").push();
             key.setValue(user);
-
-            userMap.put(user.getUid(), key.getKey());
         }
         return ResponseEntity.ok("Users created");
     }
